bda3de18838bfb57091137ac93bde075159a2171
scian0204 <scian0204@gmail.com>
2023-04-03 23:49:08 +0900

    Apply Project Changes

--- src/main/java/com/daelim/dorandoranbackend/controller/HomeController.java
+++ src/main/java/com/daelim/dorandoranbackend/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.daelim.blogbackend.controller;
+package com.daelim.dorandoranbackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
