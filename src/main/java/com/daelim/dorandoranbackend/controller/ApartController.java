2ddd881553050ac39e8b3340612ab516cc20ca61
scian0204 <scian0204@gmail.com>
2023-05-26 13:56:29 +0900

    Change get Ho list API to GET method
    호 목록 API GET방식으로 변경

--- src/main/java/com/daelim/dorandoranbackend/controller/ApartController.java
+++ src/main/java/com/daelim/dorandoranbackend/controller/ApartController.java
@@ -40,17 +40,8 @@ public class ApartController {
 
     @Operation(summary = "아파트 동 별 호 목록 API", description = "아파트ID와 동으로 호 목록 검색하는 API")
     @ApiResponse(description = "아파트 동 별 호 목록 리스트")
-    @PostMapping("/ho")
-    public Response<List<String>> getHoList(
-            @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    content = @Content(
-                            schema = @Schema(implementation = HoRequest.class)
-                    )
-            )
-            @RequestBody Map<String, Object> pathVars) {
-        Integer apartId = Integer.parseInt(pathVars.get("apartId").toString());
-        String dong = pathVars.get("dong").toString();
-
+    @GetMapping("/ho/{apartId}/{dong}")
+    public Response<List<String>> getHoList(@PathVariable Integer apartId, @PathVariable String dong) {
         return apartService.getHoList(apartId, dong);
     }
 
